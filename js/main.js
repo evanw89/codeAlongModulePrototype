a640f43e1e67a8db01c6d8c6c130af8648c42aba
evanw89 <evanw89@gmail.com>
2018-10-05 16:21:39 -0700

    third commit

--- js/main.js
+++ js/main.js
@@ -4,3 +4,17 @@ document.querySelector("#help").addEventListener("click", function() {
 document.querySelector("#chevron-down").addEventListener("click", function() {
 	document.querySelector(".course-sidebar-appointments").style.bottom = "-50%";
 })
+
+function checkSolution(bool) {
+	if(bool) {
+		document.querySelector("#solution").style.display = "block";
+		setTimeout(function() {
+			document.querySelector("#solution").style.opacity = "1";
+		}, 50);
+	} else {
+		document.querySelector("#solution").style.opacity = '0';
+		setTimeout(function() {
+			document.querySelector("#solution").style.display = "none";
+		}, 350);
+	}
+}
