1325e56bb2430f0c4154867720806ded3a2664ab
evanw89 <evanw89@gmail.com>
2018-10-05 16:29:58 -0700

    fifth commit

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 document.querySelector("#help").addEventListener("click", function() {
-	document.querySelector(".course-sidebar-appointments").style.bottom = "60px";
+	document.querySelector(".course-sidebar-appointments").style.bottom = "120px";
 })
 document.querySelector("#chevron-down").addEventListener("click", function() {
 	document.querySelector(".course-sidebar-appointments").style.bottom = "-50%";
