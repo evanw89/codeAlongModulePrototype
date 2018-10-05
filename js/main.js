9493acf5ca208a77f4c688433afb31d5d6a56cb6
evanw89 <evanw89@gmail.com>
2018-10-05 10:32:08 -0700

    first commit

---
+++ js/main.js
@@ -0,0 +1,6 @@
+document.querySelector("#help").addEventListener("click", function() {
+	document.querySelector(".course-sidebar-appointments").style.bottom = "60px";
+})
+document.querySelector("#chevron-down").addEventListener("click", function() {
+	document.querySelector(".course-sidebar-appointments").style.bottom = "-50%";
+})
