aa3a802948c902f2ff184fd205985ca38fdbdd9d
IrrelevantEvan <evanw89@gmail.com>
2018-10-18 23:38:35 -0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@ document.querySelector("#help").addEventListener("click", function() {
 	document.querySelector(".course-sidebar-appointments").style.bottom = "120px";
 })
 document.querySelector("#chevron-down").addEventListener("click", function() {
-	document.querySelector(".course-sidebar-appointments").style.bottom = "-50%";
+	document.querySelector(".course-sidebar-appointments").style.bottom = "-100%";
 })
 
 function checkSolution(bool) {
